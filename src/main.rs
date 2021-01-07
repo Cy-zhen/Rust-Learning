cff8b12a7b4f0d4c805512c1dabbe058b16d66f0
pengyuyan521 <18588982404@163.com>
2021-01-07 16:18:59 +0800

    modification main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-    //这是一个行注释
     println!("Hello, world!");
-    /*
-    这是一个块注释
-    
-    */
-    let x = 5 +  90 +  5;
-    println!("Is 'x' 10 or 100 ?   x = {}",x);
+    println!("Hello, world!");
+	println!("Hello, world!");
+
 }
