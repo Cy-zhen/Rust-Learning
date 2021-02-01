1cabebee81c3a1f5c27aa089fa119b0f67adf71d
pengyuyan521 <18588982404@163.com>
2021-02-01 14:45:16 +0800

    display_test1

--- display/src/main.rs
+++ display/src/main.rs
@@ -27,7 +27,7 @@ struct MinMax(i64, i64);
 
 impl fmt::Display for MinMax{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})",self.0, self.1)
+        write!(f, "({}, {})", self.0, self.1)
     }
 }
 
@@ -41,6 +41,15 @@ impl fmt::Display for Point2D{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "x: {}, y: {}", self.x, self.y)
     } 
+}
+
+#[derive(Debug)]
+struct Comlpex { real: f64, imag: f64, }
+
+impl fmt::Display for Comlpex{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "real: {}, imag: {}", self.real, self.imag)
+    }
 }
 
 fn main(){
@@ -59,9 +68,13 @@ fn main(){
     
     let point = Point2D{ x: 3.3, y: 7.2 };
 
+    let complex = Comlpex{ real: 3.3, imag: 7.2 };
+
     println!("Compare points:");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+    println!("Display: {} + {} i", point.x, point.y);
+    println!("Debug: {:?}", complex);
 }
 
 /*use std::fmt; // 导入 `fmt`
