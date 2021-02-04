8d1fe2f434de021e6368cdd938fb040ed11c9cc2
pengyuyan521 <18588982404@163.com>
2021-02-04 20:22:26 +0800

    format_learning1

---
+++ format/target/debug/deps/format.d
@@ -0,0 +1,5 @@
+D:\RustTest\greeting\format\target\debug\deps\format.exe: src\main.rs
+
+D:\RustTest\greeting\format\target\debug\deps\format.d: src\main.rs
+
+src\main.rs:
---
+++ format/target/debug/format.d
@@ -0,0 +1,1 @@
+D:\RustTest\greeting\format\target\debug\format.exe: D:\RustTest\greeting\format\src\main.rs
