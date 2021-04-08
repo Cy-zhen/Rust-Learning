2c449098cb1f22b6a40f60008fb0dd89f8984731
Cy-zhen <18588982404@163.com>
2021-04-08 16:45:22 +0800

    annotation1

---
+++ Hello/annotation/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    //let x = 5 + 5;
+    /*let x = 5 +  90 +  5;
+    println!("Is 'x' 10 or 100? x = {}", x);
+    */
+    //Is 'x' 10 or 100? x = 100
+    let x = 5 + /* 90 + */ 5;
+    println!("Is 'x' 10 or 100? x = {}", x);
+    //Is 'x' 10 or 100? x = 10
+}
